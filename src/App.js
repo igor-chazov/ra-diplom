08c293310cf1266717ac53001f71a74843129689
igor-chazov <igor-chazov@yandex.ru>
2022-02-25 13:18:48 +0500

    Добавлен basename

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import IndexPage from './pages/IndexPage';
 import CatalogPage from './pages/CatalogPage';
@@ -12,10 +12,9 @@ import Page404 from './pages/Page404';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/ra-diplom">
       <Layout>
         <Routes>
-          <Route path="/ra-diplom" element={<Navigate replace to="/" />} />
           <Route path="/" element={<IndexPage />} />
           <Route path="/catalog" element={<CatalogPage />} />
           <Route path="/about" element={<AboutPage />} />
